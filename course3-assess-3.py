9b68b3271ef33446da644d266210032bf1448b58
Jensen Loke <jensenkmloke@gmail.com>
2019-05-05 14:26:47 +0800

    Create course3-assess-3.py
    All 6 parts in 1

---
+++ course3-assess-3.py
@@ -0,0 +1,63 @@
+#Assessment: Project: OMDB and TasteDive Mashup
+
+import requests_with_caching
+import json
+
+def get_movies_from_tastedive(title):
+    endpoint = 'https://tastedive.com/api/similar'
+    param = {}
+    param['q'] = title
+    param['limit'] = 5
+    param['type'] = 'movies'
+    this_page_cache = requests_with_caching.get(endpoint, params=param)
+    return json.loads(this_page_cache.text)
+
+def extract_movie_titles(dic):
+    list = []
+    for i in dic['Similar']['Results']:
+        list.append(i['Name'])
+    return(list)
+
+def get_related_titles(titles_list):
+    list = []
+    for i in titles_list:
+        new_list = extract_movie_titles(get_movies_from_tastedive(i))
+        for i in new_list:
+            if i not in list:
+                list.append(i)
+    print(list)
+    return list
+
+def get_movie_data(title):
+    endpoint = 'http://www.omdbapi.com/'
+    param = {}
+    param['t'] = title
+    param['r'] = 'json'
+    this_page_cache = requests_with_caching.get(endpoint, params=param)
+    return json.loads(this_page_cache.text)
+
+# some invocations that we use in the automated tests; uncomment these if you are getting errors and want better error messages
+# get_movie_rating(get_movie_data("Deadpool 2"))
+
+def get_movie_rating(data):
+    rating = 0
+    for i in data['Ratings']:
+        if i['Source'] == 'Rotten Tomatoes':
+            rating = int(i['Value'][:-1])
+            #print(rating)
+    return rating 
+
+def get_sorted_recommendations(list):
+    new_list = get_related_titles(list)
+    new_dict = {}
+    for i in new_list:
+        rating = get_movie_rating(get_movie_data(i))
+        new_dict[i] = rating
+    print(new_dict)
+    #print(sorted(new_dict, reverse=True))
+    return [i[0] for i in sorted(new_dict.items(), key=lambda item: (item[1], item[0]), reverse=True)]
+
+    
+
+# some invocations that we use in the automated tests; uncomment these if you are getting errors and want better error messages
+# get_sorted_recommendations(["Bridesmaids", "Sherlock Holmes"])
